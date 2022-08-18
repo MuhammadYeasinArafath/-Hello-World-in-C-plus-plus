2494ba533d6e1aaec1789372b113355c659c5f2d
MuhammadYeasinArafath <mdyeasinarafathofficial@gmail.com>
2022-08-19 02:44:17 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# -Hello-World-in-C-plus-plus
+“Hello, World!” in C++.
+<br>
+1st program in C++.
---
+++ hello.cpp
@@ -0,0 +1,18 @@
+// C++ program to display "Hello World"
+  
+// Header file for input output functions
+#include <iostream>
+using namespace std;
+  
+// Main() function: where the execution of program begins
+int main()
+{
+    // prints hello world!
+    cout << "Hello World!";
+  
+    return 0;
+}
+
+//applicable in this file only
+//type {g++ ./hello.cpp} to debug in terminal.
+//type {./a.exe} to run the code.
